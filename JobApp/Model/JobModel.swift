c42976e0cfdc1a45bfb9803c4148d2d70d2982fb
Harpreetkaur70 <90725247+Harpreetkaur70@users.noreply.github.com>
2022-04-01 03:32:30 +0530

    Update JobModel.swift

--- JobApp/Model/JobModel.swift
+++ JobApp/Model/JobModel.swift
@@ -6,5 +6,24 @@
 import Foundation
 
 struct JobModel {
- 
+ let slug: String
+  let companyName: String
+  let title: String
+  let description: String
+  let remote: String
+  let url: String
+  let location: String
+  let createdAt: String
+  
+
+    init(slug: String, companyName: String, title: String,description: String, remote: String, url: String, location : String, createdAt : String) {
+        self.slug = slug
+        self.companyName = companyName
+        self.title = title
+        self.description = description
+        self.remote = remote
+        self.url = url
+        self.location = location
+        self.createdAt = createdAt
+       }
 }
