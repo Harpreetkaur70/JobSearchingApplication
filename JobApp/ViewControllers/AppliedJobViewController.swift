24bb5ffc44531b205f09c62aa258e513e721cd3b
Binayprit Singh <97624917+binayprit-2nd-semester@users.noreply.github.com>
2022-04-01 03:35:42 +0530

    Update AppliedJobViewController.swift
    Show applied jobs from core data in tabelView

--- JobApp/ViewControllers/AppliedJobViewController.swift
+++ JobApp/ViewControllers/AppliedJobViewController.swift
@@ -8,6 +8,7 @@ import UIKit
 class AppliedJobViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var jobTableView: UITableView
+     var jobArray = [Jobs]();
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +20,10 @@ class AppliedJobViewController: UIViewController, UITableViewDataSource, UITable
         
         self.jobTableView.dataSource = self;
         self.jobTableView.delegate = self;
+        
+         self.jobArray = DatabaseHelper.shareInstance.getJobs();
+        
+        self.jobTableView.reloadData();
         
     }
     
@@ -40,7 +45,7 @@ class AppliedJobViewController: UIViewController, UITableViewDataSource, UITable
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return 0;
+        return self.jobArray.count;
         
     }
 
@@ -48,7 +53,11 @@ class AppliedJobViewController: UIViewController, UITableViewDataSource, UITable
     {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "JobTableViewCell", for: indexPath) as! JobTableViewCell
-       
+        cell.selectionStyle = .none
+        let rowData = self.jobArray[indexPath.row];
+        cell.titleLabel.text = rowData.title;
+        cell.nameLabel.text = rowData.name;
+        cell.slugLabel.text = rowData.slug;
         
         return cell
         
