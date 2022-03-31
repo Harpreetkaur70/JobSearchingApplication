996c6c0e9501c02940edde869bfe8913b0411575
Harpreetkaur70 <90725247+Harpreetkaur70@users.noreply.github.com>
2022-04-01 03:14:06 +0530

    Update DatabaseHelper.swift
    Create register and login user function using core data

--- JobApp/DatabaseHelper.swift
+++ JobApp/DatabaseHelper.swift
@@ -13,6 +13,41 @@ class DatabaseHelper {
    static var shareInstance = DatabaseHelper();
     let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext;
     
+    func save(object : [String:String]) {
+        let user = NSEntityDescription.insertNewObject(forEntityName: "User", into: context!) as! User;
+        
+        user.userName = object["userName"];
+        user.password = object["password"];
+        user.mobile = object["mobile"];
+        do {
+            try context?.save();
+        }catch {
+            print("data not saved");
+        }
+    }
     
+    func getUser(userName : String, pass : String) -> Bool {
+        var result : Bool = false;
+        var userList = [User]();
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "User");
+        do{
+            userList = try context?.fetch(fetchRequest) as! [User]
+            
+            if(userList.count>0) {
+                for temp in userList {
+                    if(temp.userName == userName && temp.password == pass) {
+                        result =  true;
+                        break;
+                    } else {
+                        result = false;
+                    }
+                }
+            }
+            
+        } catch {
+            print("no user");
+        }
+        return result;
+    }
 }
 
