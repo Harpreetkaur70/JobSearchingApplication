b8a9b54b39e12a6da5e57033064d8322b0484999
Harpreetkaur70 <90725247+Harpreetkaur70@users.noreply.github.com>
2022-04-01 02:50:09 +0530

    Update LoginViewController.swift
    Implement validation and alert for Login screen

--- JobApp/ViewControllers/LoginViewController.swift
+++ JobApp/ViewControllers/LoginViewController.swift
@@ -22,10 +22,45 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func registerBtnAction(_ sender: Any) {
-     
+     let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let vc = storyBoard.instantiateViewController(withIdentifier: "RegisterViewController") as! RegisterViewController
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated:true, completion:nil)
     }
     @IBAction func loginBtnAction(_ sender: Any) {
+        if(self.userNameField.text == "") {
+            self.showAlert(message1: "Please enter username", key: "");
+        } else if(self.passwordField.text == "" ){
+            self.showAlert(message1: "Please enter password", key: "");
+        } else {
+            
+        }
+
+    }
+    
+    func showAlert(message1 : String, key : String) {
         
+        let alert = UIAlertController(title: "Alert", message: message1, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            switch action.style{
+                case .default:
+                    if(key == "exit") {
+                        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+                        let vc = storyBoard.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+                        vc.modalPresentationStyle = .fullScreen
+                        self.present(vc, animated:true, completion:nil)
+                    }
+                    
+                
+                case .cancel:
+                print("cancel")
+                
+                case .destructive:
+                print("destructive")
+                
+            }
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
