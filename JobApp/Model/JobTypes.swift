06eab2608da9f0880e8081aa52422682bcc2ee7d
Binayprit Singh <97624917+binayprit-2nd-semester@users.noreply.github.com>
2022-04-01 03:31:22 +0530

    Update JobTypes.swift
    Add job type model

--- JobApp/Model/JobTypes.swift
+++ JobApp/Model/JobTypes.swift
@@ -8,6 +8,29 @@ import Foundation
 
 struct JobTypes {
  
-    
+     let slug: Int
+  let companyName: Int
+  let title: String
+  let description: String
+  let remote: Bool
+  let url: String
+  let tag: String
+  let job_type: String
+  let location: String
+  let createdAt: String
+  
+
+    init(id: Int, userId: Int, name: String,urlVideo: String, description: String, thumbnailImage: String,createdAt: String, deletedAt: String, updatedAt : String) {
+        self.id = id
+        self.userId = userId
+        self.name = name
+        self.urlVideo = urlVideo
+        self.description = description
+        self.thumbnailImage = thumbnailImage
+        self.createdAt = createdAt
+        self.deletedAt = deletedAt
+        self.updatedAt = updatedAt
+        
+       }
 
 }
